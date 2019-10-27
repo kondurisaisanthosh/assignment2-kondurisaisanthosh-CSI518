a9fac6aac494443fbe0d284b2db7be4ee0a2352c
kondurisaisanthosh <konduri.santosh96@gmail.com>
2019-10-27 19:22:34 -0400

    lastchanges

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var app = express();
 var fs=require('fs');
 var port=5000;
 var request = require('request');
-app.use(express.static(path.join(__dirname,'/client/dist/testproj/')));
+app.use(express.static(path.join(__dirname,'/Client/dist/testproj/')));
 
 const api = require('./routes/api.js');
 app.use('/api', api);
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/dist/testproj/index.html'));
+    res.sendFile(path.join(__dirname,'./Client/dist/testproj/index.html'));
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
